1a29720fa295cd309647ddb3b476d32f4a3ef83d
lynchmaniac <lynchmaniac@gmail.com>
2016-11-27 21:09:03 +0100

    Troisième fonction

--- src/test.js
+++ src/test.js
@@ -1,4 +1,4 @@
-// Ceic est le fichier d'origine
+// Ceci est le fichier d'origine
 
 function v16_1() {
 	console.log('ceci est ma première fonction');
@@ -6,4 +6,8 @@ function v16_1() {
 
 function v16_2(test) {
 	console.log("Deuxième fonction");
+}
+
+function v16_3(controller) {
+	console.log("Troisième fonction");
 }
